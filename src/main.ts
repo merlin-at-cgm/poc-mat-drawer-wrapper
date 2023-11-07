82c45eb8fa366c815e52799bf042e94dfbe65824
Merlin Köhler <merlin.koehler@cgm.com>
2023-11-07 15:48:41 +0100

    add explicit zone js import for stackblitz

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
+import 'zone.js/dist/zone'; // Avoid error in StackBlitz
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
