8409af8ddda31ce1beaa6ea62818566a85663263
Merlin Köhler <merlin.koehler@cgm.com>
2023-11-07 15:18:09 +0100

    drawer host

--- src/app/drawer/drawer-host/drawer-host.component.ts
+++ src/app/drawer/drawer-host/drawer-host.component.ts
@@ -1,13 +1,27 @@
-import { Component, TemplateRef } from '@angular/core';
+import { Component, TemplateRef, ViewChild } from '@angular/core';
+import { MatDrawer } from '@angular/material/sidenav';
 import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-drawer-host',
   templateUrl: './drawer-host.component.html',
-  styleUrls: ['./drawer-host.component.scss']
+  styleUrls: ['./drawer-host.component.scss'],
+  providers: [{
+    provide: DrawerHostComponent,
+    useExisting: DrawerHostComponent,
+  }]
 })
 export class DrawerHostComponent {
 
   drawerTemplate$ = new BehaviorSubject<TemplateRef<HTMLElement> | null>(null);
 
+  @ViewChild('drawer') drawer: MatDrawer | undefined;
+
+  setTemplate(template: TemplateRef<HTMLElement>): void {
+    this.drawerTemplate$.next(template);
+  }
+
+  toggle(): void {
+    this.drawer?.toggle();
+  }
 }
