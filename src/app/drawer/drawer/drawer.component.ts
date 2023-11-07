5ac903153350025bdbc6ad164c28b4cd5bd21150
Merlin Köhler <merlin.koehler@cgm.com>
2023-11-07 15:21:57 +0100

    drawer component

--- src/app/drawer/drawer/drawer.component.ts
+++ src/app/drawer/drawer/drawer.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, TemplateRef, ViewChild } from '@angular/core';
+import { DrawerHostComponent } from '../drawer-host/drawer-host.component';
 
 @Component({
   selector: 'app-drawer',
   templateUrl: './drawer.component.html',
   styleUrls: ['./drawer.component.scss']
 })
-export class DrawerComponent {
+export class DrawerComponent implements AfterViewInit{
 
+  constructor(private drawerHost: DrawerHostComponent) { }
+
+  @ViewChild('drawerTemplate', {static: true}) drawerTemplate: TemplateRef<HTMLElement> | undefined;
+
+  ngAfterViewInit(): void {
+    if(!this.drawerTemplate) {
+      return;
+    }
+
+    this.drawerHost.setTemplate(this.drawerTemplate);
+  }
+
+  toggle(): void{
+    this.drawerHost.toggle();
+  }
 }
